ceab2c54ff3f9034980d49fa1e4a926e9e56cb41
Mownikaa <103503730+Mownikaa@users.noreply.github.com>
2022-09-19 16:45:02 +0200

    Update middleware.ts

--- src/Midleware/middleware.ts
+++ src/Midleware/middleware.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 const middleware = (req: Request, res: Response, next: NextFunction) => {
-  console.log("middleware");
+ 
   next();
 };
 
