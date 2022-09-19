f08ecedd4dec925b481da45b18a140daffcaf5a6
Mownikaa <103503730+Mownikaa@users.noreply.github.com>
2022-09-19 23:57:42 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,8 @@ import middleware from "./Midleware/middleware";
 
 const port = 8000;
 const app = express();
-app.use(express.json());
-
- app.use("/users", middleware, userRoute);
-
+  app.use(express.json());
+  app.use("/users", middleware, userRoute);
     app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
   });
