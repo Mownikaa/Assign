688974da1374eaea8b28a7d2cb2d397588ef28c6
Mownikaa <103503730+Mownikaa@users.noreply.github.com>
2022-09-19 16:43:28 +0200

    Update Use.ts

--- src/Controllers/Use.ts
+++ src/Controllers/Use.ts
@@ -3,31 +3,24 @@ import { NextFunction, Response, Request } from "express";
 const users = [
     { id: 1, name: "Andrea", age: 3 },
     { id: 2, name: "Tom", age: 4 },
-    { id: 3, name: "Gabriel", age: 5 },
+    { id: 3, name: "Jerry", age: 5 },
   ];
-  export const getAllUsers = (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => {
+  export const getAllUsers = (req: Request, res: Response, next: NextFunction) => {
     res.json(users);
   };
 
-  export const getUserById = (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => {
+  export const getUserById = (req: Request, res: Response, next: NextFunction) => {
     const user = users.find((user) => user.id === Number(req.params.id));
     res.json(user);
   };
+
   export const createUser = (req: Request, res: Response, next: NextFunction) => {
     const user = req.body;
     users.push(user);
     console.log(user)
-    res.json(user);
-  
+    res.json(user);  
   };
+
   export const updateUser = (req: Request, res: Response, next: NextFunction) => {
   const update = req.body;
   const userId = Number(req.params.id);
@@ -41,6 +34,7 @@ const users = [
   console.log(finduser, "After");
   res.status(200).json(finduser);
 };
+
 export const deleteUser = (req: Request, res: Response, next: NextFunction) => {
   const list = users.findIndex((user) => user.id === Number(req.params.id))
  if (list >= 0) {
