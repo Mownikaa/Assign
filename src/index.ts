bef30e17fd9623de2aaedcae4d864d40c3336fb2
Mownikaa <103503730+Mownikaa@users.noreply.github.com>
2022-09-19 23:30:00 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
 import express from "express";
-import dotenv from "dotenv";
 import userRoute from "./routes/user";
 import middleware from "./Midleware/middleware";
 
 const port = 8000;
 const app = express();
-app.use(express.json());
-dotenv.config();
-
+ app.use(express.json());
  app.use("/users", middleware, userRoute);
 
     app.listen(port, () => {
